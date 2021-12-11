9d743bddf33e448bb2666e65fd1981f702fd7517
coppx <fudaichao2133521@gmail.com>
2021-12-11 19:46:12 +0800

    add shared_lock declaration

--- src/MShared_Mutex.h
+++ src/MShared_Mutex.h
@@ -54,5 +54,46 @@ namespace M
 
         void unlock_shared();
     };
+
+    template<typename Mutex>
+    class shared_lock
+    {
+    public:
+        typedef Mutex mutex_type;
+
+        shared_lock() noexcept;
+        explicit shared_lock(mutex_type& m);
+        shared_lock(mutex_type& m, M::defer_lock_t);
+        shared_lock(mutex_type& m, M::try_to_lock_t);
+        shared_lock(mutex_type& m, M::adopt_lock_t);
+
+        template<typename Clock, typename Duration>
+        shared_lock(mutex_type& m, time_point<Clock, Duration>& abs_time);
+
+        template<typename Rep, typename Period>
+        shared_lock(mutex_type& m, duration<Rep, Period>& rel_time);
+
+        ~shared_lock();
+
+        shared_lock(const shared_lock&) = delete;
+        shared_lock& operator=(const shared_lock&) = delete;
+
+        void lock();
+        bool try_lock();
+        template<typename Rep, typename Period>
+        bool try_lock_for(const duration<Rep, Period>& rel_time);
+
+        template<typename Clock, typename Duration>
+        bool try_lock_until(const time_point<Clock, Duration>& abs_time);
+
+        void unlock();
+
+        void swap(shared_lock& u) noexcept;
+        mutex_type* release() noexcept;
+
+        bool owns_lock() const noexcept;
+        explicit operator bool() const noexcept;
+        mutex_type* mutex() const noexcept;
+    };
 }
 #endif //MTHREAD_MSHARED_MUTEX_H
