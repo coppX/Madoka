70b5e6821235880cc4b6449672cb3c55a9397b5b
coppx <fudaichao2133521@gmail.com>
2021-12-11 21:58:37 +0800

    add promise declaration

---
+++ src/MFuture.h
@@ -0,0 +1,43 @@
+//
+// Created by 最上川 on 2021/12/11.
+//
+
+#ifndef MTHREAD_MFUTURE_H
+#define MTHREAD_MFUTURE_H
+namespace M
+{
+    template<typename T>
+    class future;
+
+    class exception_ptr;
+    
+    struct allocator_arg_t { explicit allocator_arg_t() = default; };
+
+    template<typename R>
+    class promise
+    {
+    public:
+        promise();
+        template<typename Allocator>
+        promise(allocator_arg_t, const Allocator& a);
+        promise(promise&& rhs) noexcept;
+        promise& operator=(promise&& rhs) noexcept;
+
+        promise(const promise&) = delete;
+        promise& operator=(const promise&) = delete;
+
+        ~promise();
+        void swap(promise& other) noexcept;
+
+        future<R> get_future();
+
+        void set_value(const R& r);
+        void set_value(R&& r);
+        void set_exception(exception_ptr p);
+
+        void set_value_at_thread_exit(const R& r);
+        void set_value_at_thread_exit(R&& r);
+        void set_exception_at_thread_exit(exception_ptr p);
+    };
+}
+#endif //MTHREAD_MFUTURE_H
